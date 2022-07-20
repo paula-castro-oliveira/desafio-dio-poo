ed88849b1fddf86bbeb20ee62e6a163535d2b64d
paula-castro-oliveira <paulaflea@gmail.com>
2022-07-19 22:34:42 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -22,10 +21,44 @@ public class Main {
         mentoria.setData(LocalDate.now());
 
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
 
+        Dev devPaula = new Dev();
+        devPaula.setNome("Paula");
+        devPaula.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos" + devPaula.getConteudosInscritos());
+
+        devPaula.progredir();
+        devPaula.progredir();
+
+        System.out.println("--");
+        System.out.println("Conteúdos Inscritos" + devPaula.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos" + devPaula.getConteudosConcluidos());
+        System.out.println("XP: " + devPaula.calcularTotalXp());
+
+        System.out.println("--------------");
+
+        Dev devDyego = new Dev();
+        devDyego.setNome("Dyego");
+        devDyego.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos" + devDyego.getConteudosInscritos());
+
+        devDyego.progredir();
+        devDyego.progredir();
+        devDyego.progredir();
+
+        System.out.println("--");
+        System.out.println("Conteúdos Inscritos" + devDyego.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos" + devDyego.getConteudosConcluidos());
+        System.out.println("XP: " + devDyego.calcularTotalXp());
     }
 }
